909b3a173036823fd717d3261e4f973417c4c560
AungKyawSoeOo <iamaprogrammer123@gamil.com>
2023-01-15 22:00:59 +0630

    original state

--- bib_project_starter-main_akso/src/app.ts
+++ bib_project_starter-main_akso/src/app.ts
@@ -1,5 +1,5 @@
 
-import express, { Express, Request, Response, NextFunction } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -10,17 +10,9 @@ import pet_route from "./routes/PetRoute.js";
 import pet_user_route from "./routes/PetUserRoute.js";
 import cookieParser from "cookie-parser"
 import methodOverride from "method-override";
-import passport from "passport";
-import session from "express-session";
-import flash from "connect-flash";
-import renderRoute from "./routes/RenderRoute";
-import authRoute from "./routes/AuthRoute";
-import userRoute from "./routes/UserRoute";
-import "./config/Passport";
-import resetPassRoute from "./routes/ResetPassRoute";
 
 dotenv.config();
-const app: Express = express();
+const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.set("view engine", "ejs");
@@ -35,29 +27,9 @@ app.use(methodOverride('_method'));
 app.use(cookieParser());
 //db
 connectDB();
-app.use(
-  session({
-    secret: String(process.env.SECRET_KEY),
-    saveUninitialized: true,
-    resave: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  next();
-});
-
-//render route
-app.use("/", renderRoute);
 
 app.use("/admin/pets/", pet_route);
-app.use("/", authRoute);
-app.use("/users", userRoute);
-app.use("/forgot-password", resetPassRoute);
+
 app.use("/", pet_user_route);
 
 app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
--- bib_project_starter-main_akso/src/routes/UserRoute.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router } from "express";
-import { verifyEmail } from "../controllers/UserController";
-
-const userRoute: Router = express.Router();
-
-//verify email
-userRoute.get("/:id/verify/:token", verifyEmail);
-export default userRoute;
