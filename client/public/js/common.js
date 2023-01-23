9391f860f12c1659811f7fcafa542729ff5e0f3e
AungKyawSoeOo <iamaprogrammer123@gamil.com>
2023-01-23 18:51:45 +0630

    pagination added

--- client/public/js/common.js
+++ client/public/js/common.js
@@ -92,6 +92,23 @@ $('.slider').slick({
   
   });
    
+    //pagination pet page
+var items=$(".home-main-all .pet-user-all");
+var numItems = items.length;
+var perPage = 8;
+items.slice(perPage).hide();
+
+$('#pagination-container').pagination({
+  items: numItems,
+  itemsOnPage: perPage,
+  prevText: "&laquo;",
+  nextText: "&raquo;",
+  onPageClick: function (pageNumber) {
+      var showFrom = perPage * (pageNumber - 1);
+      var showTo = showFrom + perPage;
+      items.hide().slice(showFrom, showTo).show();
+  }
+});
 });
 
 //dropdown
@@ -164,6 +181,7 @@ fileInput.addEventListener("change", (event) => {
 
     reader.readAsDataURL(file);
 });
+
 
 
 
