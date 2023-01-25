12e6f1d09c9b277364e38d0c3138210e1e9f0082
DevIdol <devidol.mm@gmail.com>
2023-01-25 16:31:19 +0630

    add fav in pet details on 25 Jan 2023

--- src/services/PetUserService.ts
+++ src/services/PetUserService.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import PetModel from "../models/PetModel.js";
 import mongoose from "mongoose";
+import User from '../models/UserModel';
 import jwtDecode from "jwt-decode";
+import FavoriteModel from "../models/FavoriteModel.js";
 
 export const petDetailUserService = async (
   req: Request,
@@ -10,9 +12,16 @@ export const petDetailUserService = async (
 ) => {
   const token = req.cookies.access_token;
   let user: any;
+  let petId: any[] = [];
   if (token) {
     const decoded: any = jwtDecode(token);
     user = decoded.user;
+    await User.findById(user._id).populate("favorites");
+    let Fav = await FavoriteModel.find({ userId: user._id });
+    Fav.map((p: any) => {
+      let pid = String(p.pets);
+      petId.push(pid);
+    });
   }
   if (mongoose.Types.ObjectId.isValid(req.params.id)) {
     try {
@@ -20,7 +29,7 @@ export const petDetailUserService = async (
       if (!pet) {
         res.render("not-found", { message: "Pet Not Found!" });
       }
-      res.render("pet-detail", { pet: pet, token, user });
+      res.render("pet-detail", { pet: pet, token, user, petId });
     } catch (err: any) {
       res.render("not-found", { error: "Something Wrong!" });
     }
